e94f7391eaeccaac5abdf94359652db82b380902
Evandro Sacramento II <evandro-dev@outlook.com>
2025-05-27 13:49:47 -0300

    "Update Class Game and create application-test.properties, application-test.properties e import.sql"

--- src/main/java/com/essjr/gamelist/entities/Game.java
+++ src/main/java/com/essjr/gamelist/entities/Game.java
@@ -1,28 +1,36 @@
 package com.essjr.gamelist.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
 
 import java.util.Objects;
 
 @Entity
+@Table(name = "tb_game")
 public class Game {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-    private Integer year;
+    private Integer gameYear;
     private String genre;
     private String plataform;
     private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
 
     public Game() {    }
 
-    public Game(Long id, String title, Integer year, String genre, String plataform, String imgUrl) {
+    public Game(Long id, String title, Integer gameYear, String genre, String plataform, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
-        this.year = year;
+        this.gameYear = gameYear;
         this.genre = genre;
         this.plataform = plataform;
         this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
     }
 
     public Long getId() {
@@ -41,12 +49,12 @@ public class Game {
         this.title = title;
     }
 
-    public Integer getYear() {
-        return year;
+    public Integer getGameYear() {
+        return gameYear;
     }
 
-    public void setYear(Integer year) {
-        this.year = year;
+    public void setGameYear(Integer gameYear) {
+        this.gameYear = gameYear;
     }
 
     public String getGenre() {
@@ -73,6 +81,22 @@ public class Game {
         this.imgUrl = imgUrl;
     }
 
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -90,7 +114,7 @@ public class Game {
         return "Game{" +
                 "id=" + id +
                 ", title='" + title + '\'' +
-                ", year=" + year +
+                ", gameYear=" + gameYear +
                 ", genre='" + genre + '\'' +
                 ", plataform='" + plataform + '\'' +
                 ", imgUrl='" + imgUrl + '\'' +
