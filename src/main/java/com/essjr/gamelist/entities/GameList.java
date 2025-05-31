f8c486a8d1c0dbcb765611eb1b6f6d68f58bcc44
Evandro Sacramento <evandro-dev@outlook.com>
2025-05-30 23:50:59 -0300

    "Implementation GameList"

--- src/main/java/com/essjr/gamelist/entities/GameList.java
+++ src/main/java/com/essjr/gamelist/entities/GameList.java
@@ -1,4 +1,58 @@
 package com.essjr.gamelist.entities;
 
+import jakarta.persistence.Entity;
+
+import java.util.Objects;
+
+@Entity
 public class GameList {
+
+    @Id
+    private Long id;
+    private String name;
+
+
+    public GameList() {
+    }
+
+    public GameList(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        GameList gameList = (GameList) o;
+        return Objects.equals(id, gameList.id) && Objects.equals(name, gameList.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "GameList{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
 }
