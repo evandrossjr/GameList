f139852052c206e8f8bf7b99b73ecc313f90ce7d
Evandro Sacramento <evandro-dev@outlook.com>
2025-06-01 23:54:57 -0300

    "GameListRepository"

--- src/main/java/com/essjr/gamelist/repositories/GameListRepository.java
+++ src/main/java/com/essjr/gamelist/repositories/GameListRepository.java
@@ -1,4 +1,6 @@
 package com.essjr.gamelist.repositories;
 
-public class GameListRepository {
+import com.essjr.gamelist.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long>{
 }
